8e6b3f6fe3c2d61d67b1b1d4d7fcca41792a97f7
yang.liu <liuyang8218@163.com>
2019-05-03 18:53:09 +0800

    turbine 集群监控， 聚合 hystrix 监控数据，

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ sping cloud 微服务
  >> 2. microservice-commodity 商品服务
  >> 2. microservice-commodity-price 价格服务
  >> 2. microservice-hystrix-dashboard 可视化监控数据
+ >> 2. microservice-hystrix-turbine 集群监控，聚合hystrix监控数据
---
+++ microservice-hystrix-turbine/src/main/java/com/yang/microservicehystrixturbine/MicroserviceHystrixTurbineApplication.java
@@ -0,0 +1,15 @@
+package com.yang.microservicehystrixturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableTurbine
+@SpringBootApplication
+public class MicroserviceHystrixTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceHystrixTurbineApplication.class, args);
+	}
+
+}
